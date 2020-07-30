44c93d31064dc176fa589abe8ffb621c32caedd6
dydtjq94 <62597465+dydtjq94@users.noreply.github.com>
2020-07-30 17:53:21 +0900

    semi

--- js/submit.js
+++ js/submit.js
@@ -14,7 +14,6 @@ const mentorsSubmit = [
   {
     name: "허다원",
     nameEng: "dawon",
-
     majorNum: 1, //0이면 의예과 1이면 일반학과
     title: "학교 생활 열심히 해서 학생부 100%로 연세대 수석입학하는 비법",
   },
@@ -86,6 +85,59 @@ const mentorsSubmit = [
   },
 ];
 
+const submitUpper = document.querySelector(`.submit__upper`);
+
+const temp = decodeURI(location.href);
+const name = temp.split(":")[2];
+const major = parseInt(temp.split(":")[1].split("?")[1], 10);
+console.log(name, major);
+
+let resultNum = 0;
+let i = 0;
+
+for (;;) {
+  if (mentorsSubmit[i].name === name) {
+    resultNum = i;
+    break;
+  }
+  i++;
+}
+
+console.log(resultNum);
+
+submitUpper.innerHTML = `<div class="submit__upper__column">
+<img
+  class="submit__mentor__img"
+  src="./img/${mentorsSubmit[resultNum].nameEng}.jpeg"
+  alt=""
+/>
+</div>
+<div class="submit__upper__column">
+<div class="submit__column">
+  <div class="submit__price">
+    <div class="submit__name__title">멘토링 진행 멘토</div>
+    <input
+      type="text"
+      name="mentor"
+      class="submit__mentor"
+      placeholder="학생 이름"
+      value="${mentorsSubmit[resultNum].name}"
+      readonly
+    /><span class="submit__class">
+      멘토님
+    </span>
+  </div>
+</div>
+<div class="submit__column">
+  <div class="submit__price">
+    <div class="submit__name__title">멘토링 클래스</div>
+    <div class="submit__class">
+      ${mentorsSubmit[resultNum].title}
+    </div>
+  </div>
+</div>
+</div>`;
+
 const form = document.querySelector(".submitForm");
 const submitName = document.querySelector(`.submit__name`);
 const submitTel = document.querySelector(`.submit__tel`);
@@ -95,9 +147,7 @@ const submitBtn = document.querySelector(".submit__submit");
 const blackLoading = document.querySelector(".black__loading");
 const thx = document.querySelector(`#thx`);
 
-console.dir(submitBtn);
-document.querySelector(`.submit__submit`).disabled = true;
-
+submitBtn.disabled = true;
 let s1 = "";
 let s2 = "";
 let s3 = "";
@@ -105,6 +155,18 @@ let s4 = "";
 function handleNameChange() {
   console.dir(submitName.value);
   s1 = submitName.value;
+  let setI = setInterval(function () {
+    console.log(s1, s2, s3, s4);
+    if (s1 !== "" && s2 !== "" && s3 !== "" && s4 !== "") {
+      submitBtn.style.backgroundColor = "#0123b4";
+      submitBtn.style.color = "white";
+      document.querySelector(`.submit__submit`).disabled = false;
+    }
+
+    if (document.querySelector(`.submit__submit`).disabled === false) {
+      clearInterval(setI);
+    }
+  }, 1000);
 }
 
 function handleTelChange() {
@@ -120,20 +182,35 @@ function handleStuChange() {
 function handleConChange() {
   console.dir(submitCon.value);
   s4 = submitCon.value;
+  let setI = setInterval(function () {
+    console.log(s1, s2, s3, s4);
+    if (s1 !== "" && s2 !== "" && s3 !== "" && s4 !== "") {
+      submitBtn.style.backgroundColor = "#0123b4";
+      submitBtn.style.color = "white";
+      document.querySelector(`.submit__submit`).disabled = false;
+    }
+
+    if (document.querySelector(`.submit__submit`).disabled === false) {
+      clearInterval(setI);
+    }
+  }, 800);
 }
 
 function handleConClick() {
   s4 = "yes";
-}
+  let setI = setInterval(function () {
+    console.log(s1, s2, s3, s4);
+    if (s1 !== "" && s2 !== "" && s3 !== "" && s4 !== "") {
+      submitBtn.style.backgroundColor = "#0123b4";
+      submitBtn.style.color = "white";
+      document.querySelector(`.submit__submit`).disabled = false;
+    }
 
-setInterval(function () {
-  console.log(s1, s2, s3, s4);
-  if (s1 !== "" && s2 !== "" && s3 !== "" && s4 !== "") {
-    submitBtn.style.backgroundColor = "#0123b4";
-    submitBtn.style.color = "white";
-    document.querySelector(`.submit__submit`).disabled = false;
-  }
-}, 100);
+    if (document.querySelector(`.submit__submit`).disabled === false) {
+      clearInterval(setI);
+    }
+  }, 800);
+}
 
 submitName.addEventListener("change", handleNameChange);
 submitTel.addEventListener("change", handleTelChange);
@@ -145,33 +222,27 @@ console.dir(thx.style.display);
 
 setInterval(function () {
   if (thx.style.display === "block") {
+    blackLoading.style.opacity = 0;
     setTimeout(function () {
-      location.href = "home.html";
-    }, 3000);
+      blackLoading.classList.add("none");
+    }, 310);
+
+    setTimeout(function () {
+      location.href =
+        "passbook.html?" +
+        ":" +
+        "parent" +
+        ":" +
+        s1 +
+        ":" +
+        mentorsSubmit[resultNum].majorNum +
+        ":" +
+        name;
+    }, 312);
   }
 }, 200);
 
-const temp = decodeURI(location.href);
-const name = temp.split(":")[2];
-const major = parseInt(temp.split(":")[1].split("?")[1], 10);
-console.log(name, major);
-
-const handleCopy = (e) => {
-  console.log(e);
-  console.log(target);
-  target.select();
-  document.execCommand("Copy");
-  //   snackBar.style.opacity = 1;
-  snackBar.classList.add("snackbar__anim");
-  setTimeout(function () {
-    console.log(`gg`);
-    // snackBar.style.opacity = 0;
-    snackBar.classList.remove("snackbar__anim");
-  }, 3000);
-};
-
-function handleSubmit(e) {
-  console.log(e);
+function handleSubmit() {
   setTimeout(function () {
     blackLoading.classList.remove("none");
     console.log(blackLoading.classList);
@@ -179,52 +250,14 @@ function handleSubmit(e) {
   setTimeout(function () {
     blackLoading.style.opacity = 1;
   }, 10);
-  setTimeout(function () {
-    blackLoading.style.opacity = 0;
-  }, 3500);
+
   setTimeout(function () {
     blackLoading.classList.add("none");
-  }, 4510);
+  }, 10000);
 }
 
 function init() {
   submitBtn.addEventListener("click", handleSubmit);
-  //   form.innerHTML = `
-  //   <div class="passbook">
-  //         무통장입금
-  //         <br />
-  //         예금주: 야미구 (주)<br />계좌은행: 기업은행 <br/>계좌번호: 066-095403-04-016
-  //   ${
-  //     major === 0
-  //       ? `<div class="submit__price">149,000원</div>`
-  //       : `<div class="submit__price">99,000원</div>`
-  //   }
-  //   </div>
-  //     <form
-  //       action="https://script.google.com/macros/s/AKfycbzMJEI0x4Mxzyfd2a5RNo-rh69KxHdtW-6Z2c-QEonOg6L5zso/exec"
-  //       class="gform"
-  //       method="POST"
-  //       data-email="example@email.net"
-  //     >
-  //       <input type="text" name="message" value="${name}" />
-  //       <div class="input__radio">
-  //       <input type="radio" name="name" value="대학교 캠퍼스 ${name}" /> 캠퍼스
-  //       <input type="radio" name="name" value="장소 협의" /> 장소 협의
-  //       </div>
-  //       <input type="text" name="message" placeholder="학생 이름" />
-  //       <input
-  //         type="tel"
-  //         name="email"
-  //         pattern="[0-9]{3}-[0-9]{4}-[0-9]{4}"
-  //         placeholder="부모님 전화번호"
-  //       />
-  //       <input type="submit" value="멘토링 신청" />
-  //       <div style="display: none;" class="thankyou_message">
-  //         <!-- You can customize the thankyou message by editing the code below -->
-  //         <h2>
-  //           감사합니다! 입금 확인 완료 후에 멘토링 진행 연락 드리겠습니다!
-  //         </h2>
-  //       </div>`;
 }
 
 init();
