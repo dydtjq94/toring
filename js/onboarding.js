7011bc4f00ac6de54ca3e43052aba238bb9bbdb3
dydtjq94 <62597465+dydtjq94@users.noreply.github.com>
2020-08-14 12:31:31 +0900

    onboarding

--- js/onboarding.js
+++ js/onboarding.js
@@ -1,6 +1,7 @@
 const onboard1 = document.querySelector(".onboarding1");
 const onboard2 = document.querySelector(".onboarding2");
 const onboard3 = document.querySelector(".onboarding3");
+const onboard4 = document.querySelector(".onboarding4");
 const buttonOnboard = document.querySelector(".button__onboarding");
 const onboCount = document.querySelector(".onbo__count");
 
@@ -11,7 +12,7 @@ function handleOnboarding() {
   setTimeout(function () {
     onboard2.classList.remove("none");
     onboard2.classList.add("rightIn");
-    onboCount.innerHTML = "2/3";
+    onboCount.innerHTML = "2/4";
   }, 3);
 
   onboard1.classList.add("leftOut");
@@ -22,8 +23,8 @@ function handleOnboarding2() {
   setTimeout(function () {
     onboard2.classList.remove("rightIn");
     onboard2.classList.add("leftOut");
-    onboCount.innerHTML = "3/3";
-    buttonOnboard.innerHTML = "우리 아이 멘토 찾기";
+    onboCount.innerHTML = "3/4";
+    buttonOnboard.innerHTML = "다음";
   }, 3);
 
   setTimeout(function () {
@@ -31,6 +32,23 @@ function handleOnboarding2() {
     onboard3.classList.add("rightIn");
   }, 3);
   onboard2.classList.remove("rightIn");
+
+  buttonOnboard.addEventListener("click", handleOnboarding3);
+}
+
+function handleOnboarding3() {
+  setTimeout(function () {
+    onboard3.classList.remove("rightIn");
+    onboard3.classList.add("leftOut");
+    onboCount.innerHTML = "4/4";
+    buttonOnboard.innerHTML = "우리 아이 멘토 찾기";
+  }, 3);
+
+  setTimeout(function () {
+    onboard4.classList.remove("none");
+    onboard4.classList.add("rightIn");
+  }, 3);
+  onboard3.classList.remove("rightIn");
 
   buttonOnboard.addEventListener("click", handleGoHome);
 }
