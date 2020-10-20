6aee4fe422a97c0dee48118e78f86cbef14240d5
dydtjq94 <62597465+dydtjq94@users.noreply.github.com>
2020-10-20 11:43:59 +0900

    footer add

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -8,7 +8,7 @@ sass.compiler = require("node-sass");
 
 const routes = {
   css: {
-    watch: "src/scss/pages/*",
+    watch: "src/scss/partials/*",
     src: "src/scss/styles.scss",
     dest: "dest/css",
   },
