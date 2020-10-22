a90af9eb852c97b1e0494aae8ba80072e27ae90a
dydtjq94 <62597465+dydtjq94@users.noreply.github.com>
2020-10-22 16:52:28 +0900

    qna test

--- js/submitStudent.js
+++ js/submitStudent.js
@@ -1,7 +1,7 @@
 const form = document.querySelector(".submitForm");
-const submitName = document.querySelector(`.submit__qnaSchool`);
+const submitQnaSchool = document.querySelector(`.submit__qnaSchool`);
 const submitTel = document.querySelector(`.submit__tel`);
-const submitStu = document.querySelector(`.submit__qnaMajor`);
+const submitQnaMajor = document.querySelector(`.submit__qnaMajor`);
 const submitCon = document.querySelector(`.submit__content`);
 const submitBtn = document.querySelector(".submit__submit");
 const submitBtnPar = document.querySelector(`.submit__button`);
@@ -18,10 +18,9 @@ let s3 = "";
 let s4 = "";
 
 function handleNameChange() {
-  s1 = submitName.value;
+  s1 = submitCon.value;
   let setI = setInterval(function () {
-    console.log(s1, s2, s3, s4);
-    if (s1 !== "" && s2 !== "" && s3 !== "" && s4 !== "") {
+    if (s1 !== "" && s4 !== "") {
       submitBtn.style.backgroundColor = "#0123b4";
       submitBtn.style.color = "white";
       document.querySelector(`.submit__submit`).disabled = false;
@@ -33,18 +32,10 @@ function handleNameChange() {
   }, 1000);
 }
 
-function handleTelChange() {
-  s2 = submitTel.value;
-}
-
-function handleStuChange() {
-  s3 = submitStu.value;
-}
-
 function handleConChange() {
-  s4 = submitCon.value;
+  s4 = submitTel.value;
   let setI = setInterval(function () {
-    if (s1 !== "" && s2 !== "" && s3 !== "" && s4 !== "") {
+    if (s1 !== "" && s4 !== "") {
       submitBtn.style.backgroundColor = "#0123b4";
       submitBtn.style.color = "white";
       document.querySelector(`.submit__submit`).disabled = false;
@@ -62,7 +53,7 @@ function handleConChange() {
 function handleConClick() {
   s4 = "yes";
   let setI = setInterval(function () {
-    if (s1 !== "" && s2 !== "" && s3 !== "" && s4 !== "") {
+    if (s1 !== "" && s4 !== "") {
       submitBtn.style.backgroundColor = "#0123b4";
       submitBtn.style.color = "white";
       document.querySelector(`.submit__submit`).disabled = false;
@@ -106,11 +97,9 @@ function handleSubmit() {
 }
 
 function handleClickSubmit() {
-  submitName.addEventListener("change", handleNameChange);
-  submitTel.addEventListener("change", handleTelChange);
-  submitStu.addEventListener("change", handleStuChange);
-  submitCon.addEventListener("change", handleConChange);
-  submitCon.addEventListener("click", handleConClick);
+  submitCon.addEventListener("change", handleNameChange);
+  submitTel.addEventListener("change", handleConChange);
+  submitTel.addEventListener("click", handleConClick);
   submitBtnPar.addEventListener("click", handleSubmit);
 }
 
